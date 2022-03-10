a1be7d1f972176d84fa9331b674ba22e6cf0e2d7
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-03-10 07:41:53 +0300

    specified db context

--- BottleTracking-API/Program.cs
+++ BottleTracking-API/Program.cs
@@ -1,8 +1,18 @@
+using Data.Concrete.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<BottleTrackingDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection"))
+       .UseSnakeCaseNamingConvention();
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
