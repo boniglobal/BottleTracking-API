f85c1468120bec7b851ebc97215b5c2d0bac1700
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-03-10 07:38:19 +0300

    mapped entities.

---
+++ Data/Concrete/Contexts/Mapping/StationLogMap.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.Concrete.Contexts.Mapping
+{
+    public class StationLogMap : IEntityTypeConfiguration<StationLog>
+    {
+        public void Configure(EntityTypeBuilder<StationLog> builder)
+        {
+            builder.Property(x => x.Id).UseIdentityAlwaysColumn();
+
+            builder.Property(x => x.CreateDate).HasDefaultValueSql("now() at time zone 'utc'");
+        }
+    }
+}
