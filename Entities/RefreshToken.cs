89999286f2adabbcb463cebb6c5fab409b7066ae
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-03-15 22:59:57 +0300

    added new entity: RefreshToken

---
+++ Entities/RefreshToken.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities
+{
+    public class RefreshToken : BaseEntity
+    {
+        public string Token { get; set; }
+        public string UserId { get; set; }
+        public DateTimeOffset ExpirationTime { get; set; }
+        public DateTimeOffset? RevokedTime { get; set; }
+        public string SessionId { get; set; }
+        public bool? HasLongTTL { get; set; }
+    }
+}
