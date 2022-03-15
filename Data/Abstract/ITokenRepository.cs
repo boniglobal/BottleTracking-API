19ed66992e2b99b8f920554cc4abe12b15f21664
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-03-15 23:04:10 +0300

    Added repository: TokenRepository

---
+++ Data/Abstract/ITokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+
+namespace Data.Abstract
+{
+    public interface ITokenRepository
+    {
+        RefreshToken Get(string token);
+        void RevokeTokensByUserId(string userId, string sessionId);
+        void Add(RefreshToken token);
+        void Save();
+    }
+}
