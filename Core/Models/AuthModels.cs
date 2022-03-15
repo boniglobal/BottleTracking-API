8f5dcf22cf7daf7ec1afbc815a21dfe8815cd083
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-03-15 23:11:06 +0300

    added models: AuthModels and ResponseModels

---
+++ Core/Models/AuthModels.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.Models
+{
+    public class AuthModels
+    {
+        public class AuthRequest
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public bool? RememberMe { get; set; }
+        }
+        public class AuthResponse
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string AccessToken { get; set; }
+            public DateTimeOffset TokenExpiration { get; set; }
+        }
+    }
+}
