fd6ae93080385b750a9d4807f582ab92ab5f19bc
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-03-15 23:20:36 +0300

    specified dependencies

---
+++ Business/Dependency/ServiceRegistrations.cs
@@ -0,0 +1,25 @@
+﻿using Business.Services;
+using Core.Utilities.JWT;
+using Data.Abstract;
+using Data.Concrete.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Business.Dependency
+{
+    public static class ServiceRegistrations
+    {
+        public static void AddCustomServices(this IServiceCollection services)
+        {
+            //Helpers
+            services.AddScoped<ITokenHelper, TokenHelper>();
+
+            //Services
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPaneUserService, PanelUserService>();
+
+            //Repositories
+            services.AddScoped<IPanelUserRepository, PanelUserRepository>();
+            services.AddScoped<ITokenRepository, TokenRepository>();
+        }
+    }
+}
