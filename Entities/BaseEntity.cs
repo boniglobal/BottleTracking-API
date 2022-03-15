518e3824f6f36adafe0bac9bc45232fdcf426bc1
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-03-15 22:59:35 +0300

    DateTime changed to DateTimeOffset

--- Entities/BaseEntity.cs
+++ Entities/BaseEntity.cs
@@ -3,6 +3,6 @@
     public class BaseEntity
     {
         public int Id { get; set; }
-        public DateTime CreateDate { get; set; }
+        public DateTimeOffset CreateDate { get; set; }
     }
 }
