fb0b3b6dd82b87c2ca29ba761467567e3905716f
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-03-18 12:35:33 +0300

    created controllers: Bottles, Stations

---
+++ BottleTracking-API/Controllers/BottlesController.cs
@@ -0,0 +1,70 @@
+﻿using Business.Services;
+using Core.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static Core.DTOs.Bottle;
+using static Core.Models.ResponseModels;
+
+namespace BottleTracking_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BottlesController : ControllerBase
+    {
+        private readonly IBottleService _bottleService;
+
+        public BottlesController(IBottleService bottleService)
+        {
+            _bottleService = bottleService;
+        }
+
+        [Authorize(Roles = "Admin, Panel")]
+        [HttpGet]
+        [Route("{id}")]
+        public dynamic Get(int id)
+        {
+            var data = _bottleService.GetById(id);
+            return Messaging.GetResponse(true, null, null, data);
+        }
+
+        [Authorize(Roles = "Admin, Panel")]
+        [HttpGet]
+        public dynamic GetAll([FromQuery] RequestFilter filter)
+        {
+            var data = _bottleService.GetAll(filter);
+            return Messaging.GetResponse(true, null, null, data);
+        }
+
+        [Authorize(Roles = "Admin, Panel")]
+        [Route("statistics")]
+        public dynamic GetStatistics()
+        {
+            var data = _bottleService.GetStatistics();
+            return Messaging.GetResponse(true, null, null, data);
+        }
+
+        [Authorize("Admin, Printer")]
+        [HttpPost]
+        public dynamic Add(BottleAdd bottleAdd)
+        {
+            _bottleService.Add(bottleAdd);
+            return Messaging.GetResponse(true, null, null, null);
+        }
+
+        [Authorize(Roles = "Admin, Panel")]
+        [HttpPut]
+        public dynamic Update(BottleUpdate data)
+        {
+            _bottleService.Update(data);
+            return Messaging.GetResponse(true, null, null, null);
+        }
+
+        [Authorize(Roles = "Admin, Panel")]
+        [HttpDelete]
+        public dynamic Delete([FromBody]List<int> id)
+        {
+            _bottleService.Delete(id);
+            return Messaging.GetResponse(true, null, null, null);
+        }
+    }
+}
