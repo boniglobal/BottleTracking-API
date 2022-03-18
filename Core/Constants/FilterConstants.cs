7ce6d8e44c24db1f0733c4e130afcfa5e8b0e4de
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-03-18 13:00:37 +0300

    added constants for pagination

---
+++ Core/Constants/FilterConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Constants
+{
+    public class FilterConstants
+    {
+        public const int MinPageSize = 10;
+        public const int MaxPageSize = 100;
+        public const int MinIndex = 1;
+    }
+}
