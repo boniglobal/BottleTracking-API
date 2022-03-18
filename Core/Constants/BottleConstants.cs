7a468d2d8dbc1b06313e3280e1b8bfd6e5fb0042
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-03-18 12:59:57 +0300

    added constants for Bottle management

---
+++ Core/Constants/BottleConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Constants
+{
+    public class BottleConstants
+    {
+        public enum UsageStatus
+        {
+            Valid = 1,
+            Expired,
+            Trash
+        }
+
+        public enum BottleTypes
+        {
+            L12 = 1
+        }
+
+        //Total days
+        public const int BottleShelfLife = 1826;
+    }
+}
