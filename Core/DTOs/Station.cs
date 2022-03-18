cbe66d41ee873643f9928af84e47be1e968991de
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-03-18 13:03:30 +0300

    added dtos for Station

---
+++ Core/DTOs/Station.cs
@@ -0,0 +1,36 @@
+﻿namespace Core.DTOs
+{
+    public class Station
+    {
+        public class StationListView
+        {
+            public int Id { get; set; }
+            public DateTimeOffset CreateDate { get; set; }
+            public string ProductionLine { get; set; }
+            public int Location { get; set; }
+            public int PanelUserId { get; set; }
+            public string Fullname { get; set; }
+        }
+
+        public class StationAdd
+        {
+            public string ProductionLine { get; set; }
+            public int Location { get; set; }
+            public int PanelUserId { get; set; }
+        }
+
+        public class StationUpdate
+        {
+            public int StationId { get; set; }
+            public string ProductionLine { get; set; }
+            public int Location { get; set; }
+            public int PanelUserId { get; set; }
+        }
+
+        public class StationStatistics
+        {
+            public int TotalNumberOfLines { get; set; }
+            public int TotalNumberOfStations { get; set; }
+        }
+    }
+}
