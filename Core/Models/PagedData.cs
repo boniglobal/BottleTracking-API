9ba07385a599a313a21e72ecec3b562ce7cf1fed
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-03-18 13:04:29 +0300

    added Paging model

---
+++ Core/Models/PagedData.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Models
+{
+    public class PagedData<T>
+    {
+        public List<T> Items { get; set; }
+        public bool Previous { get; set; }
+        public bool Next { get; set; }
+        public int PageNumber { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
