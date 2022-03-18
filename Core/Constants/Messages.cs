5ac3dad5f902721c9d6414ee99df89d503580003
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-03-18 13:01:38 +0300

    added error messages for station management

---
+++ Core/Constants/Messages.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Constants
+{
+    public static class Messages
+    {
+        // Station Management error messages
+        public const string AssignDifferentUserType = "A kiosk user cannot be registered to more than one station.";
+        public const string UserNotFound = "User not found.";
+        public const string AssignUserMoreThanOneStation = "A kiosk user cannot be registered to more than one station.";
+    }
+}
