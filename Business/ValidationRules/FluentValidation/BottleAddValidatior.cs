f49b94eac5c39b4cfe0d254662d6df817e695cf1
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-03-18 12:48:38 +0300

    added validators for add and update bottle

---
+++ Business/ValidationRules/FluentValidation/BottleAddValidatior.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using static Core.DTOs.Bottle;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class BottleAddValidator : AbstractValidator<BottleAdd>
+    {
+        public BottleAddValidator()
+        {
+            RuleFor(x=>x.ProductionDate).NotEmpty();
+        }
+    }
+}
