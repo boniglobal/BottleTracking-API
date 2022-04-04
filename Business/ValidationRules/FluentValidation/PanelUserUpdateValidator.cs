6c3459bf6df0415308d913c1c7852515e645a0e0
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-04-04 13:15:06 +0300

    removed existing email and format control

--- Business/ValidationRules/FluentValidation/PanelUserUpdateValidator.cs
+++ Business/ValidationRules/FluentValidation/PanelUserUpdateValidator.cs
@@ -1,5 +1,4 @@
-﻿using Business.Services;
-using Core.Constants;
+﻿using Core.Constants;
 using FluentValidation;
 using static Core.DTOs.User;
 
@@ -7,10 +6,8 @@ namespace Business.ValidationRules.FluentValidation
 {
     public class PanelUserUpdateValidator : AbstractValidator<PanelUserUpdateRequest>
     {
-        private readonly IPaneUserService _panelUserService;
-        public PanelUserUpdateValidator(IPaneUserService panelUserService)
+        public PanelUserUpdateValidator()
         {
-            _panelUserService = panelUserService;
 
             RuleFor(x => x.Name).NotEmpty();
             RuleFor(x => x.Name).MaximumLength(UserConstants.MaxNameLength);
@@ -19,17 +16,9 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(x => x.Surname).MaximumLength(UserConstants.MaxNameLength);
 
             RuleFor(x => x.Email).NotEmpty();
-            RuleFor(x => x.Email).Matches(@"^([\w\.\-]+)@((?!\.|\-)[\w\-]+)((\.(\w){2,3})+)$");
-            RuleFor(x => x.Email).Must(CheckUserEmailAddress).WithMessage(Messages.NonUniqueEmail);
 
             RuleFor(x => x.UserType).NotEmpty();
             RuleFor(x => x.UserType).IsInEnum();
-        }
-
-        private bool CheckUserEmailAddress(PanelUserUpdateRequest dto, string email)
-        {
-            var user = _panelUserService.GetByEmail(email);
-            return user == null || user.Id == dto.Id;
         }
     }
 }
