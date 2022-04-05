cb7ae872230e4ee83e04a442cd244bf1e16945b0
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-04-05 10:45:45 +0300

    database level controls moved from validators to service

--- Business/Services/StationService.cs
+++ Business/Services/StationService.cs
@@ -1,6 +1,10 @@
-﻿using Core.Models;
+﻿using Business.Utilities;
+using Core.Constants;
+using Core.Models;
 using Data.Abstract;
 using Entities;
+using System.Net;
+using static Core.Constants.UserConstants;
 using static Core.DTOs.Station;
 
 namespace Business.Services
@@ -17,14 +21,20 @@ namespace Business.Services
     public class StationService : IStationService
     {
         private readonly IStationRepository _stationRepository;
-
-        public StationService(IStationRepository stationRepository)
+        private readonly IPaneUserService _paneUserService;
+        public StationService(
+            IStationRepository stationRepository, 
+            IPaneUserService paneUserService)
         {
             _stationRepository = stationRepository;
+            _paneUserService = paneUserService;
         }
 
         public void Add(StationAdd station)
         {
+            CheckIfUserIsAssignedToAnotherStation(station.PanelUserId);
+            CheckIfUserExistsOrItsTypeIsKiosk(station.PanelUserId);
+
             _stationRepository.Add(station);
         }
 
@@ -50,7 +60,35 @@ namespace Business.Services
 
         public void Update(StationUpdate station)
         {
+            CheckIfUserIsAssignedToAnotherStation(station.PanelUserId, station.StationId);
+            CheckIfUserExistsOrItsTypeIsKiosk(station.PanelUserId);
+
             _stationRepository.Update(station);
+        }
+
+        private void CheckIfUserIsAssignedToAnotherStation(int userId, int? stationId = null)
+        {
+            var existingStation = _stationRepository.GetByPanelUserId(userId);
+            if (stationId == null && existingStation != null)
+            {
+                throw new CustomException(Messages.AssignUserMoreThanOneStation, HttpStatusCode.BadRequest);
+            }else if(stationId != null && existingStation != null && existingStation.Id != stationId)
+            {
+                throw new CustomException(Messages.AssignUserMoreThanOneStation, HttpStatusCode.BadRequest);
+            }
+        }
+
+        private void CheckIfUserExistsOrItsTypeIsKiosk(int userId)
+        {
+            var user = _paneUserService.GetById(userId);
+            if (user == null)
+            {
+                throw new CustomException(Messages.UserNotFound, HttpStatusCode.BadRequest);
+            }
+            else if (user.Type != (int)Types.Kiosk)
+            {
+                throw new CustomException(Messages.AssignDifferentUserType, HttpStatusCode.BadRequest);
+            }
         }
     }
 }
