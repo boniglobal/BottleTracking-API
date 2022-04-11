4e840a34fd8e9f8ecb073895e481de668ecfc464
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-04-11 23:19:55 +0300

    specified swagger dependencies

---
+++ BottleTracking-API/Dependencies/Swagger.cs
@@ -0,0 +1,50 @@
+﻿using BottleTracking_API.Helpers;
+using Core.Constants;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace BottleTracking_API.Dependencies
+{
+    public static class Swagger
+    {
+        public static void AddSwaggerConfigurations(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = DocumentTexts.AuthDesc
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                        new string[] {}
+                    }
+                });
+
+                c.SchemaFilter<CustomSchemeFilters>();
+                c.UseInlineDefinitionsForEnums();
+                c.EnableAnnotations();
+
+                var xmlFile = $"{Assembly.GetEntryAssembly().GetName().Name}.XML";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath, true);
+                c.IncludeXmlComments($@"{AppDomain.CurrentDomain.BaseDirectory}\Core.xml");
+            });
+        }
+    }
+}
