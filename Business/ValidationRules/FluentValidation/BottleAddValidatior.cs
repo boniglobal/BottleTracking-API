79565e6ba951f4d9132c533104426439d3434fd9
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-04-21 23:14:39 +0300

    added a new rule to bottle add & update validators: ProductionDate cannot be greater than today

--- Business/ValidationRules/FluentValidation/BottleAddValidatior.cs
+++ Business/ValidationRules/FluentValidation/BottleAddValidatior.cs
@@ -9,15 +9,16 @@ namespace Business.ValidationRules.FluentValidation
         public BottleAddValidator()
         {
             RuleFor(x => x.ProductionDate).NotEmpty();
-            RuleFor(x => x.ProductionDate).Must(CheckDateIfNotValid).WithMessage(Messages.InValidDateFormat);
+            RuleFor(x => x.ProductionDate).Must(CheckDateIfNotValid).WithMessage(Messages.InValidDate);
             RuleFor(x => x.BottleType).NotEmpty();
             RuleFor(x => x.BottleType).IsInEnum();
         }
 
         private bool CheckDateIfNotValid(string value)
         {
-            return DateTimeOffset.TryParseExact(value, BottleConstants.ProductionDateFormat,
-                null, System.Globalization.DateTimeStyles.None, out _);
+            var format =  DateTimeOffset.TryParseExact(value, BottleConstants.ProductionDateFormat,
+                null, System.Globalization.DateTimeStyles.None, out var date);
+            return format && date.Date <= DateTimeOffset.UtcNow.Date;
         }
     }
 }
