539672a99c07ecaf47ab41cd01ca349fef2ebccb
Uğur Timurçin <ugurtimurcin@boniglobal.com>
2022-05-20 10:23:07 +0300

    added new controller and endpoint to save logs

---
+++ BottleTracking-API/Controllers/LogsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static Core.Models.ResponseModels;
+
+namespace BottleTracking_API.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogsController : ControllerBase
+    {
+        private readonly ILogger<LogsController> _logger;
+
+        public LogsController(ILogger<LogsController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpPost]
+        public dynamic Add()
+        {
+            return Messaging.GetResponse(true, null, null, null);
+        }
+    }
+}
